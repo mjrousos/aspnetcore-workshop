8404aa450501d347200f805e24ce1a7224b1ac48
Charles Lowell <chlowe@microsoft.com>
2017-08-26 13:09:15 -0700

    update lab 3 for VS2017 15.3

--- Labs/Code/Lab3/src/Lab3/Startup.cs
+++ Labs/Code/Lab3/src/Lab3/Startup.cs
@@ -6,22 +6,18 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 
 namespace Lab3
 {
     public class Startup
     {
-        public IConfigurationRoot Configuration { get; private set; }
-
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            Configuration = new ConfigurationBuilder()
-                    .SetBasePath(env.ContentRootPath)
-                    .AddJsonFile("appsettings.json")
-                    .Build();
+            Configuration = configuration;
         }
+
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -30,10 +26,8 @@ namespace Lab3
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -43,9 +37,9 @@ namespace Lab3
 
             app.Run(async (context) =>
             {
-                //await context.Response.WriteAsync($"Hello World! {env.EnvironmentName}");
                 await context.Response.WriteAsync($"{Configuration["message"]}");
             });
         }
     }
 }
+`
