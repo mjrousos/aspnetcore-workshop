7d4c5d71f43e5a57904783015dc666cfb850e04c
Charles Lowell <chlowe@microsoft.com>
2017-08-27 13:53:38 -0700

    update Lab5 for ASP.NET Core 2.0

--- Labs/Code/Lab5/src/Lab5/Middleware/RequestIdMiddleware.cs
+++ Labs/Code/Lab5/src/Lab5/Middleware/RequestIdMiddleware.cs
@@ -6,16 +6,18 @@ public class RequestIdMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly ILogger<RequestIdMiddleware> _logger;
+    private readonly IRequestIdFactory _requestIdFactory;
 
-    public RequestIdMiddleware(RequestDelegate next, IRequestId requestId, ILogger<RequestIdMiddleware> logger)
+    public RequestIdMiddleware(RequestDelegate next, IRequestIdFactory requestIdFactory, ILogger<RequestIdMiddleware> logger)
     {
         _next = next;
         _logger = logger;
+        _requestIdFactory = requestIdFactory;
     }
 
-    public Task Invoke(HttpContext context, IRequestId requestId)
+    public Task Invoke(HttpContext context)
     {
-        _logger.LogInformation($"Request {requestId.Id} executing.");
+        _logger.LogInformation($"Request {_requestIdFactory.MakeRequestId()} executing.");
 
         return _next(context);
     }
---
+++ Labs/Code/Lab5/src/Lab5/Services/RequestIdFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+public interface IRequestIdFactory
+{
+    string MakeRequestId();
+}
+
+public class RequestIdFactory : IRequestIdFactory
+{
+    private int _requestId;
+
+    public string MakeRequestId() => Interlocked.Increment(ref _requestId).ToString();
+}
