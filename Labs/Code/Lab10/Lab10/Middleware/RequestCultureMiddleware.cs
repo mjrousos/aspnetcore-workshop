d5ebe81974a34901e8e721667696d5af2684e1ff
Connie Yau <conniey@microsoft.com>
2017-09-29 13:00:01 -0700

    Lab 10: Adding initial code

---
+++ Labs/Code/Lab10/Lab10/Middleware/RequestCultureMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab10.Middleware
+{
+    public class RequestCultureMiddleware
+    {
+        private RequestDelegate _next;
+        private RequestCultureOptions _options;
+
+        public RequestCultureMiddleware(RequestDelegate next, IOptions<RequestCultureOptions> options)
+        {
+            _next = next;
+            _options = options?.Value;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            var cultureQuery = context.Request.Query["culture"];
+
+            var requestCulture = _options.DefaultCulture;
+            if (!string.IsNullOrWhiteSpace(cultureQuery))
+            {
+                var culture = new CultureInfo(cultureQuery);
+                requestCulture = culture;
+            }
+
+            if (requestCulture != null)
+            {
+                CultureInfo.CurrentCulture = requestCulture;
+                CultureInfo.CurrentUICulture = requestCulture;
+            }
+
+            return _next(context);
+        }
+    }
+
+    public static class RequestCultureMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestCulture(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestCultureMiddleware>();
+        }
+
+        public static IApplicationBuilder UseRequestCulture(this IApplicationBuilder builder, RequestCultureOptions options)
+        {
+            return builder.UseMiddleware<RequestCultureMiddleware>(Options.Create(options));
+        }
+    }
+}
---
+++ Labs/Code/Lab10/Lab10/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using Lab10.Models;
+using System.Threading.Tasks;
+
+namespace Foo.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "NameRequired")]
+        public string Name { get; set; }
+
+        public ICollection<OrderLineItem> OrderItems { get; set; } = new HashSet<OrderLineItem>();
+
+        [NotMapped]
+        public IEnumerable<Order> Orders => OrderItems.Select(o => o.Order);
+    }
+}
