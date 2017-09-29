232b7cdaa306dab45a020ca36f800c574584fe4f
Mike Rousos <Mike@MJRousos.com>
2017-09-29 11:27:04 -0400

    Update lab 1 final code sample

---
+++ Labs/Code/Lab1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(app => app.Run(async context => await context.Response.WriteAsync("Hello world!")))
+                .Build();
+
+            host.Run();
+        }
+    }
+}
