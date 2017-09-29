723633ea3d577708b20e02ff5c2c81bcda5a9b8b
Jon Jung (WEB) <jonjung@microsoft.com>
2017-09-29 16:39:48 -0700

    Add Lab 5 code

---
+++ Labs/Code/Lab5/AspNetCoreWorkshop/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWorkshop.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool Completed { get; set; }
+        public ICollection<OrderLineItem> OrderLineItems { get; set; } = new HashSet<OrderLineItem>();
+
+        [NotMapped]
+        public IEnumerable<Product> Products => OrderLineItems.Select(o => o.Product).Distinct();
+    }
+}
---
+++ Labs/Code/Lab5/AspNetCoreWorkshop/Models/StoreSettingsOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWorkshop.Models
+{
+    public class StoreSettingsOptions
+    {
+        public string StoreName { get; set; }
+        public int StoreID { get; set; }
+        public Dictionary<string, StoreSetting> Settings { get; set; }
+    }
+}
