14c2026defbb87702aa434cce9ef2ebeecc3e3a4
Connie Yau <conniey@microsoft.com>
2017-09-29 15:23:12 -0700

    Lab10: Adding section on RazorPages

---
+++ Labs/Code/Lab10/Lab10/Pages/ProductList.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Lab10.Controllers;
+using Lab10.Data;
+using Foo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab10.Pages
+{
+    public class ProductListModel : PageModel
+    {
+        private readonly OrdersContext _ordersContext;
+
+        public ProductListModel(OrdersContext ordersContext)
+        {
+            _ordersContext = ordersContext;
+        }
+
+        public IEnumerable<Product> Products { get; private set; }
+
+        public async Task OnGetAsync()
+        {
+            Products = await _ordersContext.Products.ToListAsync().ConfigureAwait(false);
+        }
+    }
+}
