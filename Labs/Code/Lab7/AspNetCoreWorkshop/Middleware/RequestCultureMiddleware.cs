fd920498ab3ed1759f68839660b52287337d5e91
Mike Rousos <Mike@MJRousos.com>
2017-10-02 10:41:31 -0400

    Add code for lab 7

---
+++ Labs/Code/Lab7/AspNetCoreWorkshop/Middleware/RequestCultureMiddleware.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWorkshop.Middleware
+{
+    public class RequestCultureMiddleware
+    {
+        private RequestDelegate _next;
+        private RequestCultureOptions _options;
+
+        public RequestCultureMiddleware(RequestDelegate next, IOptions<RequestCultureOptions> options)
+        {
+            _next = next;
+            _options = options.Value;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            CultureInfo requestCulture = null;
+
+            var cultureQuery = context.Request.Query["culture"];
+            if (!string.IsNullOrWhiteSpace(cultureQuery))
+            {
+                requestCulture = new CultureInfo(cultureQuery);
+            }
+            else
+            {
+                requestCulture = _options.DefaultCulture;
+            }
+
+
+            if (requestCulture != null)
+            {
+                CultureInfo.CurrentCulture = requestCulture;
+                CultureInfo.CurrentUICulture = requestCulture;
+            }
+
+            // Call the next delegate/middleware in the pipeline
+            return _next(context);
+        }
+    }
+
+    public static class RequestCultureMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestCulture(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestCultureMiddleware>();
+        }
+
+        public static IApplicationBuilder UseRequestCulture(this IApplicationBuilder builder, RequestCultureOptions options)
+        {
+            return builder.UseMiddleware<RequestCultureMiddleware>(Options.Create(options));
+        }
+    }
+}
---
+++ Labs/Code/Lab7/AspNetCoreWorkshop/Startup.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using AspNetCoreWorkshop.Data;
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreWorkshop.Models;
+using AspNetCoreWorkshop.Services;
+using AspNetCoreWorkshop.Middleware;
+using Microsoft.AspNetCore.Http;
+using System.Globalization;
+
+namespace AspNetCoreWorkshop
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddXmlDataContractSerializerFormatters();
+
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" }));
+            services.Configure<StoreSettingsOptions>(Configuration.GetSection("StoreSettings"));
+
+            services.AddDbContext<StoreContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+
+            services.AddSingleton<IRequestIdFactory, RequestCounter>();
+
+            services.Configure<RequestCultureOptions>(options =>
+            {
+                options.DefaultCulture = new CultureInfo(Configuration["DefaultCulture"] ?? "en-GB");
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> startupLogger)
+        {
+            app.UseStatusCodePages("text/html", "<p>You got a <strong>{0}</strong></p>");
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(subApp =>
+                {
+                    subApp.Run(async context =>
+                    {
+                        context.Response.ContentType = "text/html";
+                        await context.Response.WriteAsync("<strong> Application error. Please contact support. </strong>");
+                        await context.Response.WriteAsync(new string(' ', 512));  // Padding for IE
+                    });
+                });
+            }
+
+            app.UseRequestCulture();
+
+            app.UseStaticFiles();
+
+            app.UseMiddleware<RequestIdMiddleware>();
+
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+
+            startupLogger.LogInformation("Application startup complete!");
+
+            //startupLogger.LogCritical("This is a critical message");
+            //startupLogger.LogDebug("This is a debug message");
+            //startupLogger.LogTrace("This is a trace message");
+            //startupLogger.LogWarning("This is a warning message");
+            //startupLogger.LogError("This is an error message");
+        }
+    }
+}
