87e9b93fc497ab109117db5d741b1b288876365e
Jon Jung (WEB) <jonjung@microsoft.com>
2017-10-02 17:24:14 -0700

    Update Lab9 instructions and add code files

---
+++ Labs/Code/Lab9/AspNetCoreWorkshop/Data/StoreContext.cs
@@ -0,0 +1,37 @@
+﻿using AspNetCoreWorkshop.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWorkshop.Data
+{
+    public class StoreContext: DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderLineItem> OrderLineItems { get; set; }
+
+        /// <summary>
+        /// This method can be used to customize ORM beahviors
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // For example, table names for .NET classes can be customized
+            modelBuilder.Entity<Order>().ToTable("Order");
+            modelBuilder.Entity<Product>().ToTable("Product");
+            modelBuilder.Entity<OrderLineItem>().ToTable("OrderLineItem");
+
+            // Or, relationships between entities could be explicitly defined
+            // or keys could be defined, etc.
+        }
+    }
+}
---
+++ Labs/Code/Lab9/AspNetCoreWorkshopTests/ProductsControllerTests.cs
@@ -0,0 +1,67 @@
+﻿using AspNetCoreWorkshop.Controllers;
+using AspNetCoreWorkshop.Data;
+using AspNetCoreWorkshop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace WorkshopTests
+{
+    [TestClass]
+    public class ProductControllerTests
+    {
+        [TestMethod]
+        public void GetReturnsListOfProducts()
+        {
+            using (var context = CreateTestStoreContext())
+            {
+                // arrange
+                var productsContoller = new ProductsController(context);
+
+                // act
+                var result = productsContoller.Get().Result;
+
+                // assert
+                CollectionAssert.AreEqual(result.ToList(), context.Products.ToList());
+            }
+        }
+
+        [TestMethod]
+        public void GetByIdWithInvalidIdReturnsNotFoundResult()
+        {
+            using (var context = CreateTestStoreContext())
+            {
+                // arrange
+                var productsContoller = new ProductsController(context);
+
+                // act
+                var result = productsContoller.Get(100).Result;
+
+                // assert
+                Assert.AreEqual(typeof(NotFoundResult), result.GetType());
+            }
+        }
+
+        private StoreContext CreateTestStoreContext()
+        {
+            var options = new DbContextOptionsBuilder<StoreContext>()
+                .UseInMemoryDatabase(databaseName: "TestProductsControllerDatabase")
+                .Options;
+
+            var context = new StoreContext(options);
+
+            var products = new Product[]
+            {
+                new Product{ Name = "Widget 1.0" },
+                new Product{ Name = "Super Widget" },
+                new Product{ Name = "Widget Mini" },
+            };
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return context;
+        }
+    }
+}
